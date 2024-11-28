0cbfe1e39502da6519d383770c39ba0c3505524b
Ashwin M <24620401+glocore@users.noreply.github.com>
2024-11-28 18:29:36 +0530

    configure tailwind

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -2,9 +2,9 @@ import { versions } from "@pulsar/preload";
 
 function App() {
   return (
-    <>
+    <div className="rounded-xl bg-gray-800 p-4 m-4 border-2 border-gray-600">
       <code>{JSON.stringify(versions, null, 2)}</code>
-    </>
+    </div>
   );
 }
 
